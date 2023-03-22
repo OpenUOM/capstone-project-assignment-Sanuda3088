ee9fe5b6e30eb9247accc2d210a230705dda969c
Sanuda3088 <sanudasenod446@gmail.com>
2023-03-22 12:28:56 +0530

    Front end done

--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -60,14 +60,14 @@ export class TeacherTableComponent implements OnInit {
     })
   }
 
-  search(value) {
+  search(value: string) {
     let foundItems = [];
     if (value.length <= 0) {
       this.getTeacherData();
     } else {
-      let b = this.teacherData.filter((teacher) => {
-        if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
-          foundItems.push(teacher)
+      this.teacherData.forEach((teacher) => {
+        if (teacher[0].name.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+          foundItems.push(teacher);
         }
       });
       this.teacherData = foundItems;
