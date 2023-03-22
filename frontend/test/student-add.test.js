b6eda6b11027b4d2d47eadbe19d4edb238172caa
Sanuda3088 <sanudasenod446@gmail.com>
2023-03-22 15:58:26 +0530

    correction 08

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -3,10 +3,9 @@ process.env.NODE_ENV = "test";
 
 fixture`Testing Student UI`
     .page`http://localhost:4401/student`
-    .pageRequestTimeout(3000)
 
 test('Testing add students', async t => {
-    
+    await t.setPageLoadTimeout(3000);
     await t.navigateTo("/dbinitialize");
 
     await t.navigateTo("/addStudent");
--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -3,10 +3,10 @@ process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
     .page`http://localhost:4401/`
-    .pageRequestTimeout(3000)
+    
 
 test('Testing add teachers', async t => {
-    
+    await t.setPageLoadTimeout(3000);
     await t.navigateTo("/dbinitialize");
 
     await t.navigateTo("/addTeacher");
