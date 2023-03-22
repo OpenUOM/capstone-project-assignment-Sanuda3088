ee417fc487677dc15e6cff1cac294892b7652c66
Sanuda3088 <sanudasenod446@gmail.com>
2023-03-22 15:24:26 +0530

    correction 05

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -51,14 +51,14 @@ export class StudentTableComponent implements OnInit {
     })
   }
 
-  search(value) {
+  search(value: string) {
     let foundItems = [];
     if (value.length <= 0) {
       this.getStudentData();
     } else {
-      let b = this.studentData.filter((student) => {
+      this.studentData.forEach((student) => {
         if (student[0].name.toLowerCase().indexOf(value.toLowerCase()) > -1) {
-          foundItems.push(student)
+          foundItems.push(student);
         }
       });
       this.studentData = foundItems;
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = async () => {
     return {
       verbose: false,
-      testTimeout: 30000,
+      testTimeout: 60000,
       maxWorkers: 1
     };
   };
