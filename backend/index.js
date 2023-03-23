5259655d4a4cb3e011ffbed861aa0a8eb243a647
Sanuda3088 <sanudasenod446@gmail.com>
2023-03-23 18:44:38 +0530

    correction 15

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const server  = require ("./server.js");
 
 if(process.env.NODE_ENV === "test"){
-  server.listen(4401, () => {
+  server.listen(8080, () => {
     console.log(
-      "Capstone Project Backend is running on http://localhost:4401"
+      "Capstone Project Backend is running on http://localhost:8080"
     );
   });
 }else{
