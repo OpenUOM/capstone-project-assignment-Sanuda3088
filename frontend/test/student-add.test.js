e7cbaba3e2e0c71f7bb27187e171089f86156b7d
Sanuda3088 <sanudasenod446@gmail.com>
2023-03-30 22:58:22 +0530

    correction 19

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Student UI`
-    .page`http://localhost:8080/student`
+    .page`http://localhost:4401/student`.pageRequestTimeout(60000);
 
 test('Testing add students', async t => {
     await t.navigateTo("/dbinitialize");
--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -2,7 +2,7 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:4401/`
+    .page`http://localhost:4401/`.pageRequestTimeout(60000);
     
 
 test('Testing add teachers', async t => {
